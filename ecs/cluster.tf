a6d5ae58fae871852623a28d1c307a815bd92645
HyungJu <sungkisa@naver.com>
2024-04-05 21:35:10 +0900

    feat: associate asg to ecs cluster

--- ecs/cluster.tf
+++ ecs/cluster.tf
@@ -1,3 +1,25 @@
 resource "aws_ecs_cluster" "vacgom-cluster" {
   name = "vacgom-cluster-terraform"
 }
+
+resource "aws_ecs_capacity_provider" "vacgom-capacity-provider" {
+  depends_on = [aws_autoscaling_group.ecs-ec2-asg]
+
+  name = "vacgom-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.ecs-ec2-asg.arn
+
+    managed_scaling {
+        status = "ENABLED"
+        target_capacity = 90
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "vacgom-cluster-capacity-providers" {
+  depends_on = [aws_ecs_capacity_provider.vacgom-capacity-provider, aws_ecs_cluster.vacgom-cluster]
+  cluster_name = aws_ecs_cluster.vacgom-cluster.name
+
+  capacity_providers = [aws_ecs_capacity_provider.vacgom-capacity-provider.name]
+}
