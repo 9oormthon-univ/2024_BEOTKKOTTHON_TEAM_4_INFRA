5db660d1159749c5158ad8590e943126d477d49f
HyungJu <sungkisa@naver.com>
2024-04-30 22:57:31 +0900

    fix: s3 bucket policy를 OAI를 위해 수정

--- s3/s3.tf
+++ s3/s3.tf
@@ -21,13 +21,8 @@ data "aws_iam_policy_document" "vacgom-policy" {
     effect    = "Allow"
     resources = ["${aws_s3_bucket.vacgom-images.arn}/*"]
     principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
     }
   }
 }
